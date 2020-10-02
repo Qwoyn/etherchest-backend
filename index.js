1fee6a57b59d435b978e3d817c4529c15dad0c9a
Daniel Pittman <dpdanpittman@gmail.com>
2020-10-02 00:17:25 -0700

    Update index.js

--- index.js
+++ index.js
@@ -453,10 +453,10 @@ function startApp() {
 
             let gemPrice = price * 1;
             
-            state.stats.prices.listed.gems.diamond = Math.ceil(gemPrice);
-            state.stats.prices.listed.gems.sapphire = Math.ceil(gemPrice / 2) + 1;
-            state.stats.prices.listed.gems.emerald = Math.ceil(gemPrice / 4) + 1;
-            state.stats.prices.listed.gems.ruby = Math.ceil(gemPrice / 10) + 1;
+            state.stats.prices.listed.gems.diamond = Math.ceil(gemPrice * 1.007);
+            state.stats.prices.listed.gems.sapphire = Math.ceil((gemPrice * 1.007) / 2) + 1;
+            state.stats.prices.listed.gems.emerald = Math.ceil((gemPrice * 1.007) / 4) + 1;
+            state.stats.prices.listed.gems.ruby = Math.ceil((gemPrice * 1.007) / 10) + 1;
 
             console.log('diamond price is ' + state.stats.prices.listed.gems.diamond);
             console.log('sapphire price is ' + state.stats.prices.listed.gems.sapphire);
