dba459fa6496a58dce91f6657ce0766da1c9205a
Daniel Pittman <dpdanpittman@gmail.com>
2020-10-02 00:21:41 -0700

    Update index.js

--- index.js
+++ index.js
@@ -327,11 +327,11 @@ app.get('/delegation/:user', (req, res, next) => {
 
 app.listen(port, () => console.log(`EtherChest API listening on port ${port}!`))
 var state;
-var startingBlock = ENV.STARTINGBLOCK || 47437400; //GENESIS BLOCKs
+var startingBlock = ENV.STARTINGBLOCK || 47438500; //GENESIS BLOCKs
 const username = ENV.ACCOUNT || 'etherchest'; //main account with all the SP
 const key = steem.PrivateKey.from(ENV.KEY); //active key for account
 const sh = ENV.sh || '';
-const ago = ENV.ago || 47437400;
+const ago = ENV.ago || 47438500;
 const prefix = ENV.PREFIX || 'etherchest_'; // part of custom json visible on the blockchain during watering etc..
 const clientURL = ENV.APIURL || 'https://api.hivekings.com' // can be changed to another node
 var client = new steem.Client(clientURL);
