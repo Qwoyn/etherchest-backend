961273cbb558db005b48ee140ea19e6e0c06ec2f
Daniel Pittman <dpdanpittman@gmail.com>
2020-10-02 23:00:56 -0700

    Update index.js

--- index.js
+++ index.js
@@ -346,7 +346,9 @@ function startApp() {
 
                             if(state.users[json.from]){
                                 state.users[json.from].gems.push(gemID)
-                                state.users[json.from][gems][gemCountNumber].push(gem)
+                                for (var i = 0; i < gemCountNumber.length; i++) {
+                                    gemCountNumber[i] = gem
+                                  }
                             } else
                             
                             //if user does not exist in db create user and db entry
