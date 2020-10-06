d9a2d2bc83f66f0be4577f0f7f454667ba654600
Daniel Pittman <dpdanpittman@gmail.com>
2020-10-06 00:19:06 -0700

    Update index.js

--- index.js
+++ index.js
@@ -395,9 +395,10 @@ function startApp() {
                             }
 
                             if(state.users[json.from]){
-                                state.users[json.from].diamond.push(gemCountNumber)
-                                state.gemList.push(gemList)
-                                state.gemList.gemCountNumber.push(diamond)
+                                state.users[json.from].diamond.push(gemList)
+                                state.users[json.from].diamond[gemList].push(diamond)
+                               // state.gemList.push(gemList)
+                              //  state.gemList.gemCountNumber.push(diamond)
                             } else
                             
                             //if user does not exist in db create user and db entry
