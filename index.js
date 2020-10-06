5f85c4ae3d07de4c65ba4e4084a1d76415d1ccdc
Daniel Pittman <dpdanpittman@gmail.com>
2020-10-06 00:33:25 -0700

    Update index.js

--- index.js
+++ index.js
@@ -161,11 +161,11 @@ app.get('/delegation/:user', (req, res, next) => {
 
 app.listen(port, () => console.log(`EtherChest API listening on port ${port}!`))
 var state;
-var startingBlock = ENV.STARTINGBLOCK || 47553500; //GENESIS BLOCK
+var startingBlock = ENV.STARTINGBLOCK || 47553800; //GENESIS BLOCK
 const username = ENV.ACCOUNT || 'etherchest'; //main account with all the SP
 const key = dhive.PrivateKey.from(ENV.KEY); //active key for account
 const sh = ENV.sh || ''; //state hash
-const ago = ENV.ago || 47553500; //supposed to be genesis block 
+const ago = ENV.ago || 47553800; //supposed to be genesis block 
 const prefix = ENV.PREFIX || 'etherchest_'; // part of custom json visible on the blockchain during watering etc..
 var client = new dhive.Client(["https://api.openhive.network"]);
 var processor;
@@ -375,11 +375,7 @@ function startApp() {
                                 v: 0
                             }
                         }
-                        
-                        var gemList = {
-                            [gemCountNumber]: {},
-                        }
-
+                
                         //assign gem qualities
                         var diamond = {
                             stone: want,
@@ -395,10 +391,8 @@ function startApp() {
                             }
 
                             if(state.users[json.from]){
-                                state.users[json.from].diamond.push(gemList)
-                                state.users[json.from].diamond[gemCountNumber].push(diamond)
-                               // state.gemList.push(gemList)
-                              //  state.gemList.gemCountNumber.push(diamond)
+                                state.users[json.from].diamond.push(gemCountNumber)
+                                state.gemList.push(diamond)
                             } else
                             
                             //if user does not exist in db create user and db entry
