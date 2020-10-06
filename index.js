8e1e48c3b058bea5fe047556e72e8deef4f16d72
Daniel Pittman <dpdanpittman@gmail.com>
2020-10-06 01:56:56 -0700

    Update index.js

--- index.js
+++ index.js
@@ -408,16 +408,7 @@ function startApp() {
                             }
                         }
 
-                        var d = parseInt(state.bal.c / 4)
-                        state.bal.r += state.bal.c
-                        if (d > 0) {
-                          //  state.refund.push(['xfer', 'ec-vault', parseInt(4 * d), 'To Validator'])
-                          //  state.bal.c -= d * 4
-                          //  d = parseInt(state.bal.c / 5) * 2
-                            state.bal.c = 0
-                        } else {
-                            state.bal.c = 0
-                        }
+                        state.bal.c = 0
 
                         const c = parseInt(amount)
                         state.bal.c += c
