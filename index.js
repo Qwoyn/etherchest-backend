a1d04ae71a32e1fd18d0ba90cc67122f9f5cfba4
Daniel Pittman <dpdanpittman@gmail.com>
2020-10-09 17:04:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -350,7 +350,7 @@ function startApp() {
     //https://app.steemconnect.com/sign/custom-json?required_auths=%5B%5D&required_posting_auths=%5B%22USERNAME%22%5D&id=qwoyn_water&json=%7B%22plants%22%3A%5B%22c35%22%5D%7D
     processor.on('register', function(json, from) {
         let usernames = json.from,
-                try {
+             
                 if (state.users[usernames] = undefined) {
                     state.users[json.from] = {
                         addrs: [],
@@ -366,9 +366,6 @@ function startApp() {
                     }
 
                    state.userCount++;
-                }
-                } catch (e){
-                state.cs[`${json.block_num}:${from}`] = `${from} user already registered`
                 }
         state.cs[`${json.block_num}:${from}`] = `${from} succesfully watered ${usernames}`
     });
