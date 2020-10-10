ca11863a1e294c4c41fdd7969a10a4fd83518554
Daniel Pittman <dpdanpittman@gmail.com>
2020-10-10 00:07:25 -0700

    Update index.js

--- index.js
+++ index.js
@@ -19,14 +19,33 @@ const ipfs = new IPFS({
  * @dev upgrading ipfs integration
  */
 
+
 const IpfsHttpClient = require('ipfs-http-client')
 const { globSource } = IpfsHttpClient
-const ipfs = IpfsHttpClient()
- 
-const ipfsFile = async e => {
-const file = await ipfs.add(globSource('./state.js', { recursive: true }))
-console.log(file)
+const IPFS = IpfsHttpClient()
+
+async function main () {
+  const node = await IPFS.create()
+  const version = await node.version()
+
+  console.log('Version:', version.version)
+
+  const fileAdded = await node.add({
+    path: 'hello.txt',
+    content: 'Hello World 101'
+  })
+
+  console.log('Added file:', fileAdded.path, fileAdded.cid)
+
+  const chunks = []
+  for await (const chunk of node.cat(fileAdded.cid)) {
+      chunks.push(chunk)
+  }
+
+  console.log('Added file contents:', uint8ArrayConcat(chunks).toString())
 }
+ 
+
  
 /*
 {
