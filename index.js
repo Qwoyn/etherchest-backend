43b4c3d07aa453c4e0525a94061292a57ec73049
Daniel Pittman <dpdanpittman@gmail.com>
2020-10-11 19:29:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -174,11 +174,11 @@ app.get('/delegation/:user', (req, res, next) => {
 
 app.listen(port, () => console.log(`EtherChest API listening on port ${port}!`))
 var state;
-var startingBlock = ENV.STARTINGBLOCK || 47719650 ; //GENESIS BLOCK
+var startingBlock = ENV.STARTINGBLOCK || 47719999 ; //GENESIS BLOCK
 const username = ENV.ACCOUNT || 'etherchest'; 
 const key = dhive.PrivateKey.from(ENV.KEY); 
 const sh = ENV.sh || ''; //state hash
-const ago = ENV.ago || 47719650; //genesis block 
+const ago = ENV.ago || 47719999; //genesis block 
 const prefix = ENV.PREFIX || 'etherchest_'; // part of custom json visible on the blockchain during watering etc..
 var client = new dhive.Client(["https://api.openhive.network", "https://api.hivekings.com"]);
 var processor;
