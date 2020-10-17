dbc1af450072b02df87c162b5e2809c8d97a203a
Daniel Pittman <dpdanpittman@gmail.com>
2020-10-16 22:25:38 -0700

    Update index.js

--- index.js
+++ index.js
@@ -176,12 +176,12 @@ hivejs.config.set('rebranded_api', true);
 hivejs.broadcast.updateOperations();
 app.listen(port, () => console.log(`EtherChest API listening on port ${port}!`))
 var state;
-var startingBlock = ENV.STARTINGBLOCK || 47859693; //GENESIS BLOCK
+var startingBlock = ENV.STARTINGBLOCK || 47866748; //GENESIS BLOCK
 const username = ENV.ACCOUNT || 'etherchest'; 
 const key = dhive.PrivateKey.from(ENV.KEY); 
 const sh = ENV.sh || ''; //state hash
-const ago = ENV.ago || 47859693; //genesis block 
-const prefix = ENV.PREFIX || 'etherchest_'; // part of custom json visible on the blockchain during watering etc..
+const ago = ENV.ago || 47866748; //genesis block 
+const prefix = ENV.PREFIX || 'etherchest_'; // part of custom json visible on the blockchain during actions etc..
 var client = new dhive.Client([
     "https://anyx.io",
     "https://api.deathwing.me",
@@ -196,7 +196,7 @@ var processor;
 var recents = [];
 
 
-//const transactor = steemTransact(client, dhive, prefix);
+const transactor = steemTransact(client, dhive, prefix);
 
 /****ISSUE****/
 //I think this is where the app can get the hash from etherchest_report that is saved in state.js and use it
