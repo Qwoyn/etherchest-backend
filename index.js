e383e988ac21e3196f583b92e11af4246b877227
Daniel Pittman <dpdanpittman@gmail.com>
2020-10-19 18:05:14 -0700

    update

--- index.js
+++ index.js
@@ -176,19 +176,13 @@ hivejs.config.set('rebranded_api', true);
 hivejs.broadcast.updateOperations();
 app.listen(port, () => console.log(`EtherChest API listening on port ${port}!`))
 var state;
-var startingBlock = ENV.STARTINGBLOCK || 47889063; //GENESIS BLOCK
+var startingBlock = ENV.STARTINGBLOCK || 47947736; //GENESIS BLOCK
 const username = ENV.ACCOUNT || 'etherchest'; 
 const key = dhive.PrivateKey.from(ENV.KEY); 
 const sh = ENV.sh || ''; //state hash
-const ago = ENV.ago || 47889063; //genesis block 
+const ago = ENV.ago || 47947736; //genesis block 
 const prefix = ENV.PREFIX || 'etherchest_'; // part of custom json visible on the blockchain during actions etc..
 var client = new dhive.Client([
-    "https://anyx.io",
-    "https://api.deathwing.me",
-    "https://api.hivekings.com",
-    "https://api.hive.blog",
-    "https://api.openhive.network",
-    "https://rpc.ausbit.dev",
     "https://hive-api.arcange.eu",
     "https://hive.roelandp.nl"
 ], {rebrandedApi: true});
