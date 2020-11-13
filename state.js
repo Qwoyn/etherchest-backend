3c2b9abef5168412444ca9b5b3c753c042534f4a
Daniel Pittman <dpdanpittman@gmail.com>
2020-11-12 22:08:43 -0800

    update

--- state.js
+++ state.js
@@ -1,173 +1,208 @@
 module.exports = {
-   "delegations": [
-      {
-         "delegator": "etherchest",
-         "vests": 0,
-      },
-   ],
-   "blocknumber": 0,
-   "userCount": 1,
-   "kudos": {},
-   "stats": {
-      "bu": "QmRGhkgH9XCUuUP8irFEpA9TSTvDVDbwKDqzEfkKc4CJjD",
-      "bi": 34160000,
-      "prices": {
-         "listed": { 
-            "a": 20000,
-            "b": 20000,
-            "c": 20000,
-            "d": 20000,
-            "e": 20000,
-            "f": 20000,
-            "t": 20000,
-            "gems": {
-               "diamond": 5000000,
-               "sapphire": 5000000,
-               "emerald": 5000000,
-               "ruby": 5000000
+      "delegations": [
+         {
+            "delegator": "etherchest",
+            "vests": 0
+         }
+      ],
+      "blocknumber": 48633345,
+      "userCount": 3,
+      "kudos": {},
+      "stats": {
+         "bu": "QmRGhkgH9XCUuUP8irFEpA9TSTvDVDbwKDqzEfkKc4CJjD",
+         "bi": 34160000,
+         "prices": {
+            "listed": {
+               "a": 20000,
+               "b": 20000,
+               "c": 20000,
+               "d": 20000,
+               "e": 20000,
+               "f": 20000,
+               "t": 20000,
+               "gems": {
+                  "diamond": 3745267,
+                  "sapphire": 1877134,
+                  "emerald": 943067,
+                  "ruby": 382627
+               },
+               "weapons": {
+                  "sword": 1,
+                  "dagger": 1,
+                  "bow": 1
+               },
+               "armor": {
+                  "shield": 1,
+                  "chestarmor": 1,
+                  "leatherboots": 1
+               },
+               "potions": {
+                  "healing": 1,
+                  "poison": 1
+               },
+               "tools": {
+                  "anvil": 1
+               }
             },
-            "weapons": {
-               "sword" : 1,
-               "dagger": 1,
-               "bow": 1,
-            },
-            "armor": {
-               "shield": 1,
-               "chestarmor": 1,
-               "leatherboots": 1,
-            },
-            "potions": {
-               "healing": 1,
-               "poison": 1,
-            },
-            "tools": {
-               "anvil": 1,
+            "purchase": {
+               "land": 19500
             }
          },
-         "purchase": {
-            "land": 19500
-         }
-      },
-      "supply": {
-         "land": {
-            "a": 499,
-            "ac": 1,
-            "b": 500,
-            "bc": 0,
-            "c": 500,
-            "cc": 0,
-            "d": 500,
-            "dc": 0,
-            "e": 500,
-            "ec": 0,
-            "f": 500,
-            "fc": 0,
-            "g": 500,
-            "gc": 0,
-            "t": 420000,
-            "tc": 1,
-            "counter": 0
-         },
-         "gems": [
-            "diamond",
-            "sapphire",
-            "emerald",
-            "ruby"
-         ],
-         "guildTypes": [
-            "miner",
-            "blacksmith",
-            "merchants",
-         ]
-      },
-      "gemCount": 0,
-      "pre_sale_diamondCount":0,
-      "pre_sale_sapphireCount": 0,
-      "pre_sale_emeraldCount": 0,
-      "pre_sale_rubyCount": 0,
-      "diamondCount":0,
-      "sapphireCount": 0,
-      "emeraldCount": 0,
-      "rubyCount": 0,
-      "stakers": 0,
-      "guilds": [
-         {
-            "name": "EtherChest",
-            "founder": "etherchest",
-            "gems": [],
-            "ducats": [],
-            "weapons": [],
-            "badges": [],
-            "mines": [
-            "a1"
+         "supply": {
+            "land": {
+               "a": 499,
+               "ac": 1,
+               "b": 500,
+               "bc": 0,
+               "c": 500,
+               "cc": 0,
+               "d": 500,
+               "dc": 0,
+               "e": 500,
+               "ec": 0,
+               "f": 500,
+               "fc": 0,
+               "g": 500,
+               "gc": 0,
+               "t": 420000,
+               "tc": 1,
+               "counter": 0
+            },
+            "gems": [
+               "diamond",
+               "sapphire",
+               "emerald",
+               "ruby"
             ],
-            "members": 1,
-            "memberNames": [
-               "etherchest"
+            "guildTypes": [
+               "miner",
+               "blacksmith",
+               "merchants"
             ]
          },
-      ]
-   },
-   "bal": {
-      "r": 0,
-      "c": 0,
-      "b": 0,
-      "p": 5000
-   },
-   "refund": [],
-   "mines": {
-      "a1": {
-         "owner": "etherchest",
-         "guildOwned": true,
-         "mineType": "Iron Mine",
-         "care": [],
-         "aff": [],
-         "stage": 1,
-         "substage": 1,
-         "traits": [
-            "beta mine"
-         ],
-         "value": 5000,
+         "gemCount": 0,
+         "pre_sale_diamondCount": 0,
+         "pre_sale_sapphireCount": 0,
+         "pre_sale_emeraldCount": 0,
+         "pre_sale_rubyCount": 0,
+         "diamondCount": 0,
+         "sapphireCount": 0,
+         "emeraldCount": 0,
+         "rubyCount": 0,
+         "stakers": 0,
+         "guilds": [
+            {
+               "name": "EtherChest",
+               "founder": "etherchest",
+               "gems": [],
+               "ducats": [],
+               "weapons": [],
+               "badges": [],
+               "mines": [
+                  "a1"
+               ],
+               "members": 1,
+               "memberNames": [
+                  "etherchest"
+               ]
+            }
+         ]
       },
-   },
-   "lands": {
-      "forSale": []
-   },
-   "land": {
-      "a1": {
-         "owner": "etherchest",
-         "mineType": "Iron Mine",
-         "care": [],
-         "aff": [],
-         "stage": 1,
-         "substage": 1,
-         "traits": [
-            "beta mine"
-         ],
-         "guilded": true,
-         "guildTreasury": "etherchest",
+      "bal": {
+         "r": 0,
+         "c": 0,
+         "b": 0,
+         "p": 5000
       },
-   },
-   "users": {
-      "etherchest": {
-         "addrs": [
-            "a1"
-         ],
-         "diamond": [],
-         "pre_sale_diamond": [],
-         "sapphire": [],
-         "pre_sale_sapphire": [],
-         "emerald": [],
-         "pre_sale_emerald": [],
-         "ruby": [],
-         "pre_sale_ruby": [],
-         "ducats" : 0,
-         "profile": [],
-         "hero": 1,
-         "guild": "etherchest",
-         "friends": []
+      "refund": [],
+      "mines": {
+         "a1": {
+            "owner": "etherchest",
+            "guildOwned": true,
+            "mineType": "Iron Mine",
+            "care": [],
+            "aff": [],
+            "stage": 1,
+            "substage": 1,
+            "traits": [
+               "beta mine"
+            ],
+            "value": 5000
+         }
       },
-   },
+      "lands": {
+         "forSale": []
+      },
+      "land": {
+         "a1": {
+            "owner": "etherchest",
+            "mineType": "Iron Mine",
+            "care": [],
+            "aff": [],
+            "stage": 1,
+            "substage": 1,
+            "traits": [
+               "beta mine"
+            ],
+            "guilded": true,
+            "guildTreasury": "etherchest"
+         }
+      },
+      "users": {
+         "etherchest": {
+            "addrs": [
+               "a1"
+            ],
+            "diamond": [],
+            "pre_sale_diamond": [],
+            "sapphire": [],
+            "pre_sale_sapphire": [],
+            "emerald": [],
+            "pre_sale_emerald": [],
+            "ruby": [],
+            "pre_sale_ruby": [],
+            "ducats": 0,
+            "profile": [],
+            "hero": 1,
+            "guild": "etherchest",
+            "friends": []
+         },
+         "qwoyn": {
+            "addrs": [],
+            "diamond": [],
+            "pre_sale_diamond": [],
+            "emerald": [],
+            "pre_sale_emerald": [],
+            "sapphire": [],
+            "pre_sale_sapphire": [],
+            "ruby": [],
+            "pre_sale_ruby": [],
+            "ducats": 0,
+            "hero": 1,
+            "guild": "",
+            "friends": [],
+            "v": 0
+         },
+         "hundredthmonkey": {
+            "addrs": [],
+            "diamond": [],
+            "pre_sale_diamond": [],
+            "emerald": [],
+            "pre_sale_emerald": [],
+            "sapphire": [],
+            "pre_sale_sapphire": [],
+            "ruby": [],
+            "pre_sale_ruby": [],
+            "ducats": 0,
+            "hero": 1,
+            "guild": "",
+            "friends": [],
+            "v": 0
+         }
+      },
       "gemList": [],
-      "cs": {},
-}
+      "cs": {
+         "48584336:qwoyn": "qwoyn succesfully registered",
+         "48586703:hundredthmonkey": "hundredthmonkey succesfully registered"
+      }
+   }
